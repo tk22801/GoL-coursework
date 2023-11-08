eed86d08a831fe8bd4b458d2e42be776e0e9f28d
tk22801 <tk22801@bristol.ac.uk>
2023-11-08 22:12:06 +0000

    Step 1 done

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,6 +1,9 @@
 package gol
 
-import "fmt"
+import (
+	"fmt"
+	"uk.ac.bris.cs/gameoflife/util"
+)
 
 type distributorChannels struct {
 	events     chan<- Event
@@ -13,12 +16,17 @@ type distributorChannels struct {
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
-	world := make([][]byte, p.ImageHeight)
-	io.Filename<-
+	fmt.Println("test")
+	fmt.Println(fmt.Sprintf("%dx%d", p.ImageWidth, p.ImageHeight))
+	filename := fmt.Sprintf("%dx%d", p.ImageWidth, p.ImageHeight)
+	c.ioCommand <- ioInput
+	c.ioFilename <- filename
+	turn := 0
+
 	//Create a 2D slice to store the world OK.
 	//0 That's not enough on its own.
 	//We actually have to get the
-	//image in so we can evolve it with
+	//image in, so we can evolve it with
 	//our game of life algorithm.
 	//Or how do we do that with the IO
 	//goroutine that we've just talked about?
@@ -33,82 +41,106 @@ func distributor(p Params, c distributorChannels) {
 	//We then get that image byte by
 	//byte and store it in this 2D world.
 
-	for i := range c.ioInput {
+	world := make([][]byte, p.ImageHeight)
+	for i := range world {
 		world[i] = make([]byte, p.ImageWidth)
 	}
-	fmt.Println(world)
-	// TODO: Create a 2D slice to store the world.
-	newWorld := make([][]byte, p.ImageHeight)
-	for i := range world {
-		newWorld[i] = make([]byte, p.ImageWidth)
+
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			val := <-c.ioInput
+			//if val != 0 {
+			//	fmt.Println(x, y)
+			//}
+			world[y][x] = val
+		}
 	}
+	// TODO: Create a 2D slice to store the world.
+	for Turn := 0; Turn < p.Turns; Turn++ {
 
-	turn := 0
-
-	// TODO: Execute all turns of the Game of Life.
-
-	for x := 0; x < p.ImageWidth; x++ {
-
-		for y := 0; y < p.ImageHeight; y++ {
-			numNeighbours := 0
-			xBack := x - 1
-			xForward := x + 1
-			yUp := y - 1
-			yDown := y + 1
-
-			if x == 0 {
-				xBack = p.ImageWidth - 1
-			}
-			if x == p.ImageWidth-1 {
-				xForward = 0
-			}
-			if y == 0 {
-				yUp = p.ImageHeight - 1
-			}
-			if y == p.ImageHeight-1 {
-				yDown = 0
-			}
-			if world[xBack][y] == 255 { //Horizontal
-				numNeighbours += 1
-			}
-			//fmt.Println("Hello 4")
-			if world[xForward][y] == 255 {
-				numNeighbours += 1
-			}
-			if world[x][yUp] == 255 { //Vertical
-				numNeighbours += 1
-			}
-			if world[x][yDown] == 255 {
-				numNeighbours += 1
-			}
-			if world[xBack][yDown] == 255 { //Diagonal
-				numNeighbours += 1
-			}
-			if world[xForward][yUp] == 255 {
-				numNeighbours += 1
-			}
-			if world[xBack][yUp] == 255 {
-				numNeighbours += 1
-			}
-			if world[xForward][yDown] == 255 {
-				numNeighbours += 1
-			}
-			if numNeighbours == 2 && world[x][y] == 255 || numNeighbours == 3 {
-				newWorld[x][y] = 255
-			} else {
-				newWorld[x][y] = 0
-			}
-
-			if p.Turns == 1 {
-				fmt.Println(x, y, newWorld[x][y])
-			}
-
+		newWorld := make([][]byte, p.ImageHeight)
+		for i := range world {
+			newWorld[i] = make([]byte, p.ImageWidth)
 		}
 
+		// TODO: Execute all turns of the Game of Life.
+
+		for x := 0; x < p.ImageWidth; x++ {
+
+			for y := 0; y < p.ImageHeight; y++ {
+				numNeighbours := 0
+				xBack := x - 1
+				xForward := x + 1
+				yUp := y - 1
+				yDown := y + 1
+
+				if x == 0 {
+					xBack = p.ImageWidth - 1
+				}
+				if x == p.ImageWidth-1 {
+					xForward = 0
+				}
+				if y == 0 {
+					yUp = p.ImageHeight - 1
+				}
+				if y == p.ImageHeight-1 {
+					yDown = 0
+				}
+				if world[xBack][y] == 255 { //Horizontal
+					numNeighbours += 1
+				}
+				//fmt.Println("Hello 4")
+				if world[xForward][y] == 255 {
+					numNeighbours += 1
+				}
+				if world[x][yUp] == 255 { //Vertical
+					numNeighbours += 1
+				}
+				if world[x][yDown] == 255 {
+					numNeighbours += 1
+				}
+				if world[xBack][yDown] == 255 { //Diagonal
+					numNeighbours += 1
+				}
+				if world[xForward][yUp] == 255 {
+					numNeighbours += 1
+				}
+				if world[xBack][yUp] == 255 {
+					numNeighbours += 1
+				}
+				if world[xForward][yDown] == 255 {
+					numNeighbours += 1
+				}
+				if numNeighbours == 2 && world[x][y] == 255 || numNeighbours == 3 {
+					newWorld[x][y] = 255
+				} else {
+					newWorld[x][y] = 0
+				}
+
+				//if p.Turns == 1 {
+				//fmt.Println(x, y, newWorld[x][y])
+				//}
+
+			}
+		}
+		turn = Turn
+		world = newWorld
 	}
-	p.Turns = p.Turns - 1
-	world = newWorld
+	turn += 1
 	// TODO: Report the final state using FinalTurnCompleteEvent.
+	aliveCells := []util.Cell{}
+	for i := 0; i < p.ImageWidth; i++ {
+		for j := 0; j < p.ImageHeight; j++ {
+			if world[i][j] == 255 {
+				newCell := []util.Cell{{j, i}}
+				aliveCells = append(aliveCells, newCell...)
+			}
+		}
+	}
+
+	c.events <- FinalTurnComplete{
+		CompletedTurns: turn, Alive: aliveCells,
+	}
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
